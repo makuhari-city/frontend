fad091b4d218e768f80e6214e150d944637f167c
Yasushi Sakai <yasushisakai+git@pm.me>
2021-07-15 06:26:53 +0900

    commenting out TakeUuid.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ListView from "./ListView";
 import Topic from "./Topic";
 import CreateTopic from "./CreateTopic";
 import EditTopic from "./EditTopic";
-import TakeUuid from "./TakeUuid";
+// import TakeUuid from "./TakeUuid";
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
       <Topic path="app/topic/:topicId" />
       <CreateTopic path="app/topic/create" />
 	  <EditTopic path="app/topic/:topicId/edit" />
-	  <TakeUuid path="app/user/:topicId/take" />
+		{/*<TakeUuid path="app/user/:topicId/take" />*/}
     </Router>
   );
 };
